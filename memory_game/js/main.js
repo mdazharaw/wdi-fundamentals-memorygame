5230477134de316405df4515bfb5a488052ab0b3
mdazharaw <darkaz62@hotmail.com>
2020-03-07 22:29:58 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,21 @@
-console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+const cards = ["queen","queen", "king", "king"];
+const cardsInPlay = [];
 
+var cardOne = "queen";
+cardsInPlay.push(cardOne);
+
+console.log("User flipped queen");
+
+var cardTwo = "king";
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped king");
+
+if (cardsInPlay.length===2){
+	if(cardsInPlay[0]===cardsInPlay[1]){
+		alert("You found a match!");
+	} 
+	else {
+		alert("Sorry, try again");
+	}
+}
